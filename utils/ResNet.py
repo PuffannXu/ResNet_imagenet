0369c01033d25beb8d1b7e6777cf91d0a4eaa6cf
xupf <xpf22@mails.tsinghua.edu.cn>
2024-12-12 09:41:09 +0800

    添加imagenet224网络结构ResNet_for_Imagenet

--- utils/ResNet.py
+++ utils/ResNet.py
@@ -306,7 +306,7 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x):
+    def forward(self, x):
         a = {}
         # See note [TorchScript super()]
         # a['in'] = x
@@ -340,30 +340,195 @@ class ResNet(nn.Module):
 
         return x, a
 
+
+class ResNet_for_Imagenet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None,
+                 qn_on: bool = False,
+                 fp_on: int = 0,
+                 weight_bit: int = 4,
+                 output_bit: int = 8,
+                 isint: int = 0,
+                 clamp_std: int = 0,
+                 quant_type: str = 'None',
+                 group_number: int = 72,
+                 left_shift_bit: int = 3):
+        super(ResNet_for_Imagenet, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        # 第一层3x3 s=2
+        # 是否考虑FP8量化及硬件特性
+        if fp_on == 1:
+            self.conv1 = Conv2d_fp8(3, self.inplanes, kernel_size=3, stride=2, padding=1, bias=False)
+        elif fp_on == 2:
+            self.conv1 = Conv2d_fp8_hw(3, self.inplanes, kernel_size=3, stride=2, padding=1, bias=False,
+                                       quant_type=quant_type, group_number=group_number, left_shift_bit=left_shift_bit)
+        elif (qn_on):
+            self.conv1 = Conv2d_quant(qn_on=qn_on, in_channels=3, out_channels=self.inplanes,kernel_size=3,stride=2, padding=1, bias=False,
+                                      weight_bit=weight_bit, output_bit=output_bit, isint=isint, clamp_std=clamp_std)
+        else:
+            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=2, padding=1, bias=False)
+        # 是否添加BN
+        if BN:
+            self.bn1 = norm_layer(self.inplanes)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block=block, planes=64, blocks=layers[0],
+                                       qn_on=qn_on,fp_on=fp_on,
+                                       weight_bit=weight_bit,output_bit=output_bit,
+                                       isint=isint, clamp_std=clamp_std,
+                                       quant_type=quant_type,group_number=group_number, left_shift_bit=left_shift_bit)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0],
+                                       qn_on=qn_on,fp_on=fp_on,
+                                       weight_bit=weight_bit,output_bit=output_bit,
+                                       isint=isint, clamp_std=clamp_std,
+                                       quant_type=quant_type,group_number=group_number, left_shift_bit=left_shift_bit)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1], qn_on=qn_on,
+                                       fp_on=fp_on,
+                                       weight_bit=weight_bit,
+                                       output_bit=output_bit,
+                                       isint=isint, clamp_std=clamp_std,
+                                       quant_type=quant_type,
+                                       group_number=group_number, left_shift_bit=left_shift_bit)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2], qn_on=qn_on,
+                                       fp_on=fp_on,
+                                       weight_bit=weight_bit,
+                                       output_bit=output_bit,
+                                       isint=isint, clamp_std=clamp_std,
+                                       quant_type=quant_type,
+                                       group_number=group_number, left_shift_bit=left_shift_bit)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if BN and zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False,
+                    qn_on: bool = False,
+                    fp_on: int = 0,
+                    weight_bit: int = 4,
+                    output_bit: int = 8,
+                    isint: int = 0,
+                    clamp_std: int = 0,
+                    quant_type: str = 'None',
+                    group_number: int = 72,
+                    left_shift_bit: int = 3):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer, qn_on=qn_on,
+                            fp_on=fp_on,
+                            weight_bit=weight_bit,
+                            output_bit=output_bit,
+                            isint=isint, clamp_std=clamp_std,
+                            quant_type=quant_type,
+                            group_number=group_number,
+                            left_shift_bit=left_shift_bit))
+
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer, qn_on=qn_on,
+                                fp_on=fp_on,
+                                weight_bit=weight_bit,
+                                output_bit=output_bit,
+                                isint=isint, clamp_std=clamp_std,
+                                quant_type=quant_type,
+                                group_number=group_number,
+                                left_shift_bit=left_shift_bit))
+
+        return nn.Sequential(*layers)
+
     def forward(self, x):
-        return self._forward_impl(x)
+        a = {}
+        x = self.conv1(x) # 112
+        if BN:
+            x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        for i in range(0, len(self.layer1)):
+            x = self.layer1[i](x)[0] # 56
+        for i in range(0, len(self.layer2)):
+            x = self.layer2[i](x)[0] # 28
+        for i in range(0, len(self.layer3)):
+            x = self.layer3[i](x)[0] # 14
+        for i in range(0, len(self.layer4)):
+            x = self.layer4[i](x)[0] # 7
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
 
-
-def _resnet(block, layers, **kwargs):
-    model = ResNet(block, layers, **kwargs)
-    return model
+        return x, a
 
 
 def ResNet18(**kwargs):
-    return _resnet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    return ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
 
 
-def ResNet34(**kwargs):
-    return _resnet(BasicBlock, [3, 4, 6, 3], **kwargs)
+def ResNet18_for_Imagenet(**kwargs):
+    return ResNet_for_Imagenet(BasicBlock, [2, 2, 2, 2], **kwargs)
 
 
-def ResNet50(**kwargs):
-    return _resnet(Bottleneck, [3, 4, 6, 3], **kwargs)
-
-
-def ResNet101(**kwargs):
-    return _resnet(Bottleneck, [3, 4, 23, 3], **kwargs)
-
-
-def ResNet152(**kwargs):
-    return _resnet(Bottleneck, [3, 8, 36, 3], **kwargs)
+# def _resnet(block, layers, **kwargs):
+#     model = ResNet(block, layers, **kwargs)
+#     return model
+#
+#
+# def ResNet34(**kwargs):
+#     return _resnet(BasicBlock, [3, 4, 6, 3], **kwargs)
+#
+#
+# def ResNet50(**kwargs):
+#     return _resnet(Bottleneck, [3, 4, 6, 3], **kwargs)
+#
+#
+# def ResNet101(**kwargs):
+#     return _resnet(Bottleneck, [3, 4, 23, 3], **kwargs)
+#
+#
+# def ResNet152(**kwargs):
+#     return _resnet(Bottleneck, [3, 8, 36, 3], **kwargs)
