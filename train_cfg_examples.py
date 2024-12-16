67b97462b429a93ddddcfe02975d9ccf12663d9b
xupf <xpf22@mails.tsinghua.edu.cn>
2024-12-16 09:29:21 +0800

    修改了硬件量化时scale的计算方式，与FP尾数的bit有关系。 共跑10个CFG，为FP8_w_hw前提下left_shift_bit = 0 or 3的实验。

--- train_cfg_examples.py
+++ train_cfg_examples.py
@@ -11,19 +11,21 @@
 # 训练20个epoch
 EPOCH = 20
 
-# ============= FP8 Software Baseline =============
-qn_on = 0
-fp_on = 1
-weight_bit = 0
-output_bit = 0
-isint = 0
-clamp_std = 0
-quant_type = "none"
-group_number = 0
-left_shift_bit = 0
+# # ============= FP8 Software Baseline =============
+# qn_on = 0
+# fp_on = 1
+# weight_bit = 0
+# output_bit = 0
+# isint = 0
+# clamp_std = 0
+# quant_type = "none"
+# group_number = 0
+# left_shift_bit = 0
+#
+# model_name = "ResNet18_imagenet_fp8_wo_hw_epoch20.pth"
 
-model_name = "ResNet18_imagenet_fp8_wo_hw_epoch20.pth"
 
+# 1
 # ============= FP8 Layer Align =============
 qn_on = 0
 fp_on = 2
@@ -35,8 +37,10 @@ quant_type = "layer"
 group_number = 0
 left_shift_bit = 0
 
-model_name = "ResNet18_imagenet_fp8_w_hw_layer_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_layer_wo_be_epoch20.pth"
 
+
+# 2
 # ============= FP8 Channel Align =============
 qn_on = 0
 fp_on = 2
@@ -48,8 +52,10 @@ quant_type = "channel"
 group_number = 0
 left_shift_bit = 0
 
-model_name = "ResNet18_imagenet_fp8_w_hw_channel_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_channel_wo_be_epoch20.pth"
 
+
+# 3
 # ============= FP8 Group9 Align =============
 qn_on = 0
 fp_on = 2
@@ -61,9 +67,11 @@ quant_type = "group"
 group_number = 9
 left_shift_bit = 0
 
-model_name = "ResNet18_imagenet_fp8_w_hw_group9_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_group9_wo_be_epoch20.pth"
 
-# ============= FP8 Group72 Baseline =============
+
+# 4
+# ============= FP8 Group72 Align =============
 qn_on = 0
 fp_on = 2
 weight_bit = 0
@@ -74,9 +82,11 @@ quant_type = "group"
 group_number = 72
 left_shift_bit = 0
 
-model_name = "ResNet18_imagenet_fp8_w_hw_group72_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_group72_wo_be_epoch20.pth"
 
-# ============= FP8 Group288 Baseline =============
+
+# 5
+# ============= FP8 Group288 Align =============
 qn_on = 0
 fp_on = 2
 weight_bit = 0
@@ -87,30 +97,106 @@ quant_type = "group"
 group_number = 288
 left_shift_bit = 0
 
-model_name = "ResNet18_imagenet_fp8_w_hw_group288_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_group288_wo_be_epoch20.pth"
 
-# ============= I8W8 Software Baseline =============
-qn_on = 1
-fp_on = 0
-weight_bit = 8
-output_bit = 8
+
+
+# ========= Adding Bit Extension to Improve Accuracy ===========
+# 6
+# ============= FP8 Layer Align with Bit Extension =============
+qn_on = 0
+fp_on = 2
+weight_bit = 0
+output_bit = 0
 isint = 0
 clamp_std = 0
-quant_type = "none"
+quant_type = "layer"
 group_number = 0
-left_shift_bit = 0
+left_shift_bit = 3 # this is changed
 
-model_name = "ResNet18_imagenet_I8W8_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_layer_w_be_epoch20.pth"
 
-# ============= I4W4 Software Baseline =============
-qn_on = 1
-fp_on = 0
-weight_bit = 4
-output_bit = 4
+
+# 7
+# ============= FP8 Channel Align with Bit Extension =============
+qn_on = 0
+fp_on = 2
+weight_bit = 0
+output_bit = 0
 isint = 0
 clamp_std = 0
-quant_type = "none"
+quant_type = "channel"
 group_number = 0
-left_shift_bit = 0
+left_shift_bit = 3
 
-model_name = "ResNet18_imagenet_I4W4_epoch20.pth"
+model_name = "ResNet18_imagenet_fp8_w_hw_channel_w_be_epoch20.pth"
+
+
+# 8
+# ============= FP8 Group9 Align with Bit Extension =============
+qn_on = 0
+fp_on = 2
+weight_bit = 0
+output_bit = 0
+isint = 0
+clamp_std = 0
+quant_type = "group"
+group_number = 9
+left_shift_bit = 3
+
+model_name = "ResNet18_imagenet_fp8_w_hw_group9_w_be_epoch20.pth"
+
+
+# 9
+# ============= FP8 Group72 Align with Bit Extension =============
+qn_on = 0
+fp_on = 2
+weight_bit = 0
+output_bit = 0
+isint = 0
+clamp_std = 0
+quant_type = "group"
+group_number = 72
+left_shift_bit = 3
+
+model_name = "ResNet18_imagenet_fp8_w_hw_group72_w_be_epoch20.pth"
+
+
+# 10
+# ============= FP8 Group288 Align with Bit Extension =============
+qn_on = 0
+fp_on = 2
+weight_bit = 0
+output_bit = 0
+isint = 0
+clamp_std = 0
+quant_type = "group"
+group_number = 288
+left_shift_bit = 3
+
+model_name = "ResNet18_imagenet_fp8_w_hw_group288_w_be_epoch20.pth"
+# # ============= I8W8 Software Baseline =============
+# qn_on = 1
+# fp_on = 0
+# weight_bit = 8
+# output_bit = 8
+# isint = 0
+# clamp_std = 0
+# quant_type = "none"
+# group_number = 0
+# left_shift_bit = 0
+#
+# model_name = "ResNet18_imagenet_I8W8_epoch20.pth"
+#
+# # ============= I4W4 Software Baseline =============
+# qn_on = 1
+# fp_on = 0
+# weight_bit = 4
+# output_bit = 4
+# isint = 0
+# clamp_std = 0
+# quant_type = "none"
+# group_number = 0
+# left_shift_bit = 0
+#
+# model_name = "ResNet18_imagenet_I4W4_epoch20.pth"
